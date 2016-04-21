cc8517e2fa98f34bb63558530fe4d3c7584d96fe
Michael Leditschke <ml019.ammd.com.au>
2016-04-21 21:39:31 +1000

    Align naming of region variables with others. Include error codes in the logs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,14 +5,14 @@ TIMESTAMP=`date --utc +%Y%m%d_%H%M%SZ`
 PGPASSWORD="$DB_PASSWORD" pg_dump -U $DB_USERNAME -p $DB_PORT -d $DB_NAME -h $DB_HOST > $TIMESTAMP.dump
 RESULT=$?
 if [ ${RESULT} -ne 0 ]; then
-    echo "Pg_dump failed, exiting..."
+    echo "pg_dump failed with exit code $RESULT, exiting..."
     exit 1
 fi
 
-aws s3 cp $TIMESTAMP.dump s3://$BUCKET_NAME/$BUCKET_PATH --region $REGION
+aws s3 cp $TIMESTAMP.dump s3://$BUCKET_NAME/$BUCKET_PATH --region $BUCKET_REGION
 RESULT=$?
 if [ ${RESULT} -ne 0 ]; then
-    echo "Copy to S3 failed, exiting..."
+    echo "Copy to S3 failed with exit code $RESULT, exiting..."
     exit 1
 fi
 
